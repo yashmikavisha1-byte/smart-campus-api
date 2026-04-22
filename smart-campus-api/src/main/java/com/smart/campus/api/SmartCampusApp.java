bd25562326f6e4e45c7875692e65ef2fbf95ca27
yashmikavisha1-byte <yashmikavisha1@gmail.com>
2026-04-22 13:31:08 +0530

    Initialize main application configuration
    This file sets up the main application class and starts the project.

--- smart-campus-api/src/main/java/com/smart/campus/api/SmartCampusApp.java
+++ smart-campus-api/src/main/java/com/smart/campus/api/SmartCampusApp.java
@@ -1,8 +1,24 @@
 package com.smart.campus.api;
 
+import java.util.HashSet;
+import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/")
 public class SmartCampusApp extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(com.smart.campus.resource.DiscoveryResource.class);
+        classes.add(com.smart.campus.resource.RoomResource.class);
+        classes.add(com.smart.campus.resource.SensorResource.class);
+        classes.add(com.smart.campus.mapper.RoomNotEmptyExceptionMapper.class);
+        classes.add(com.smart.campus.mapper.LinkedResourceNotFoundExceptionMapper.class);
+        classes.add(com.smart.campus.mapper.SensorUnavailableExceptionMapper.class);
+        classes.add(com.smart.campus.mapper.GlobalExceptionMapper.class);
+        classes.add(com.smart.campus.filter.LoggingFilter.class);
+        return classes;
+    }
 }
